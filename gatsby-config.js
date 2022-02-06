f95967bfde67c4a35b6986c202a7b26315373065
khan <khan@pop-os.localdomain>
2022-02-05 19:31:44 -0600

    added cloud

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,7 +42,8 @@ module.exports = {
     //   },
     // },
     `gatsby-plugin-sass`,
-    `gatsby-plugin-sitemap`
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-gatsby-cloud`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
