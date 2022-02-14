acc194764a704e0f797024519016504fd9954737
Imad Khan <imadalikhan@gmail.com>
2022-02-14 11:42:42 -0600

    added window check

--- src/components/page/projects/interior/BackgroundSwap.js
+++ src/components/page/projects/interior/BackgroundSwap.js
@@ -3,14 +3,15 @@ import React, {useState,useRef,useEffect, useCallback} from 'react';
 export default function BackgroundSwap({data}) {
 	const {prevBg,newBg} = data;
 	const hiddenRef = useRef();
-
+	const isBrowser = typeof window !== "undefined"
+	
 	function toggleBackgroundCLass(newClass) {
 		document.getElementsByTagName('body')[0].classList = []
 		document.getElementsByTagName('body')[0].classList.add(newClass);
 	}
 
 	const scrollHandler = useCallback(() => {
-		if (window.pageYOffset >= hiddenRef.current.offsetTop - 500) {
+		if (isBrowser && window.pageYOffset >= hiddenRef.current.offsetTop - 500) {
 			toggleBackgroundCLass(newBg);
 		} else {
 			toggleBackgroundCLass(prevBg);
@@ -19,11 +20,13 @@ export default function BackgroundSwap({data}) {
 
 
 	useEffect(() => {
-		
-		window.addEventListener('scroll', scrollHandler);
-		return () => {
-			window.removeEventListener('scroll', scrollHandler);
+		if (isBrowser) {
+			window.addEventListener('scroll', scrollHandler);
+			return () => {
+				window.removeEventListener('scroll', scrollHandler);
+			}
 		}
+
 	}, [hiddenRef, scrollHandler]);
 	
 
