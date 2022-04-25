1dd2b7e4b7f5cbc37fbc707f851f42e6034d29ee
Amit Dishoom <amit@mramitpatel.com>
2022-04-24 21:28:15 -0500

    work.json updates

--- src/components/page/projects/Hero.js
+++ src/components/page/projects/Hero.js
@@ -6,7 +6,7 @@ export default function Hero({img}) {
 			<div className="container">
 				<div className="row">
 					<div className="push-2 col-10 push-lg-hide col-lg-12">
-						<h1 className="projects-hero-title">Selected Projects</h1>
+						<h1 className="projects-hero-title">Select Projects</h1>
 						<p className="projects-hero-text">More samples available upon request.</p>
 					</div>
 				</div>
