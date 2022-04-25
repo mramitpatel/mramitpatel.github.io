b67d842916b897eb17c26e89d9ce3a4e3216210d
Amit Dishoom <amit@mramitpatel.com>
2022-04-24 22:53:49 -0500

    more edits

--- src/components/page/info/Resume.js
+++ src/components/page/info/Resume.js
@@ -5,16 +5,16 @@ const exp = [
 		year:'2016-2022',
 		company: 'The Office of Experience',
 		title: 'Creative Director',
-		description:["While at OX, I lead design iniatives across brand, product and digital projects. I was forunate to work with some of the most talented designers, strategists and practicioners in Chicago - over the course of 6 years, the team grew from 8 to 60, changed offices once, and went on 4 boat parties."]
+		description:["At OX, I lead design iniatives across brand, product and digital projects. I was forunate to work with some of the most talented designers, strategists and practicioners in Chicago - over the course of 6 years, the team grew from 8 to 60, changed offices once, and went on 4 boat parties.","I was focused on building a team of designers that were well versed in Product Design tools and process – introducing Figma and Sketch into our workflow and set the foundation for component based design.", "While at OX, I redesigned Spectrum Enterprise's web experience, led the creative marketing execution for Tate & Lyle, oversaw the design and UX of Dot Foods B2B E-Commerce platform and made many friends along the way."]
 	},
 	{
-		year:'2008–2016',
+		year:'20010–2016',
 		company: 'UnitOneNine',
 		title: 'Creative Director',
 		description:["A team of two is mightier than a team of twenty. At UON, a small, nimble team managed the redesign of large enterprise websites such as JLL.com as well making time for launching a successful marketing/design meetup group called CISC. I was fortunate to lead visual design and UX for the replatforming of Beam Brand websites as well renvisioning LasikPlus.com "]
 	},
 	{
-		year:'2006–2008',
+		year:'2006–2010',
 		company: 'SomeOddPilot, Royal Order, Sender',
 		title: 'Designer',
 		description:["Designed web, print, brand experiences for Patagonia, Mozilla, NEC and (Former) President Obama 2008 Inagural Campaign."
@@ -29,7 +29,7 @@ const ExperienceItem = ({year,company,title,description}) => {
 				<div className="push-2 col-4 push-lg-hide col-lg-12 resume-item-left">
 					<div className="resume-item-year">{year}</div>
 				</div>
-				<div className="col-6 col-lg-12 resume-item-right">
+				<div className="col-5 col-lg-12 resume-item-right">
 				<div className="resume-item-company">{company}</div>
 				<div className="resume-item-title">{title}</div>
 				<div className="resume-item-description">{description.map((d,idx) => <p key={idx}>{d}</p>)}</div>
