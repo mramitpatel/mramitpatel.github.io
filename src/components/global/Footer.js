76c8d134ba45198b45b58f931d77902ab3cf17d7
Amit Dishoom <amit@mramitpatel.com>
2022-04-27 23:03:59 -0500

    footer edits
    and work

--- src/components/global/Footer.js
+++ src/components/global/Footer.js
@@ -7,10 +7,15 @@ export default function Footer({isHome}) {
 		<div className="footer">
 		<div className="container">
 			<div className="row">
-				<a className="button scrollUp" href="#">↥</a>
-					<a href="mailto:amit@mramitpatel.com" className="button" > Email</a>
-					<a href="https://www.linkedin.com/in/mramitpatel/" className="button" target="_blank"> Linkedin</a>
-					<a href="https://twitter.com/mramitpatel"  target="_blank" className="button">Twitter</a>
+				<div class="col-2 col-md-12"><a className="button scrollUp" href="#">↥</a></div>
+				<div class="col-9 col-md-12">
+					<a href="mailto:amit@mramitpatel.com" className="button" >Email</a>
+					<a href="https://www.instagram.com/mramitpatel/" className="button" target="_blank">Instagram</a>
+					<a href="https://www.linkedin.com/in/mramitpatel/" className="button" target="_blank">Linkedin</a>
+					{/*<a href="https://twitter.com/mramitpatel"  target="_blank" className="button">Twitter</a>*/}
+					
+				</div>
+				<div class="col-1 col-md-12"><span>✌︎</span></div>
 {/*				<div className={`col-10 ${copyClassList}`}>
 					<p className="footer-copyright">&copy; Amit Patel. All trademarks are property of their respective owners.</p>
 				</div> */}
