39cbfa33c8b969f2d2ed977a1b5ccc8b2bae735b
Amit Dishoom <amit@mramitpatel.com>
2022-04-29 14:49:52 -0500

    seo.js, hero.js, info.js

--- src/pages/info.js
+++ src/pages/info.js
@@ -6,16 +6,8 @@ import Hero from '../components/page/info/Hero';
 import Resume from '../components/page/info/Resume'
 export default function Index() {
 	return (
-<<<<<<< Updated upstream
 		<Layout pageclassName="info" type="info">
 			<Seo title="Info" />
-=======
-		<Layout pageClass="info" type="info">
-			<Seo
-				title="About Amit Patel, Creative Director, Product Designer, Chicago"
-				description="Amit Patel is a hands-on creative director, product designer and brand builder; currently based in Chicago, IL"
-				/>
->>>>>>> Stashed changes
 			<Hero/>
 			<Resume />
 			<ClientExperience/>
