e49f51048ea7ac05c7df1d479ef2e20a0bdc02d9
Amit Dishoom <amit@mramitpatel.com>
2022-05-12 11:02:11 -0500

    resume updates

--- src/components/page/info/Resume.js
+++ src/components/page/info/Resume.js
@@ -5,18 +5,18 @@ const exp = [
 		year:'2016-2022',
 		company: 'The Office of Experience',
 		title: 'Creative Director',
-		description:["At OX, I lead design initiatives across brand, product and digital projects. I was fortunate to work with some of the most talented designers, strategists and practitioners in Chicago - over the course of 6 years, the team grew from 8 to 60, changed offices once, and went on 4 boat parties.","I was focused on building a team of designers that were well versed in Product Design tools and process – introduced Figma and Sketch into our workflow and set the foundation for component based design.", "While at OX, I redesigned Spectrum Enterprise's web experience, led the creative marketing execution for Tate & Lyle, oversaw the design and UX of Dot Foods B2B E-Commerce platform and made many friends along the way."]
+		description:["At OX, I led design initiatives across brand, product and digital projects. I was fortunate to work with some of the most talented designers, strategists and practitioners in Chicago. Over the course of 6 years, the team grew from 8 to 60, we changed offices once, and went on 4 boat parties.","I was focused on building a team of designers that were well-versed in Product Design tools and process – introduced Figma and Sketch into our workflow and set the foundation for component based design.", "While at OX, I redesigned Spectrum Enterprise's web experience, led the creative marketing execution for Tate & Lyle, oversaw the design and UX of Dot Foods B2B E-Commerce platform and made many friends along the way."]
 	},
 	{
 		year:'2010–2016',
 		company: 'UnitOneNine',
 		title: 'Creative Director',
-		description:["A team of two is mightier than a team of twenty. At UON, a small, nimble team managed the redesign of large enterprise websites such as JLL.com as well making time for launching a successful marketing/design meetup group called CISC. I was fortunate to lead visual design and UX for the re-platforming of Beam Brand websites as well re-imagining LasikPlus.com "]
+		description:["At UON, a small, nimble team managed the redesign of large enterprise websites such as JLL.com as well making time to launch a successful marketing/design meetup group called CISC. I was fortunate to lead visual design and UX for the re-platforming of Beam Brand websites as well re-imagining LasikPlus.com "]
 	},
 	{
 		year:'2006–2010',
 		company: 'SomeOddPilot, Royal Order, Sender',
-		title: 'Designer',
+		title: 'Freelance Designer',
 		description:["Designed web, print, brand experiences for Patagonia, Mozilla, NEC and (Former) President Obama 2008 Inaugural Campaign."
 		,""]
 	},
@@ -26,7 +26,7 @@ const ExperienceItem = ({year,company,title,description}) => {
 	return(
 		<div className="resume-item">
 			<div className="row">
-				<div className="push-2 col-4 push-lg-hide col-lg-12 resume-item-left">
+				<div className="push-2 col-4 push-lg-hide col-lg-12 col-md-4 resume-item-left">
 					<div className="resume-item-year">{year}</div>
 				</div>
 				<div className="col-5 col-lg-12 resume-item-right">
