119cdade7e81ecb3c6b5e7bf2b46af6cd09bf7a1
Amit Dishoom <amit@mramitpatel.com>
2022-05-14 19:53:39 -0500

    GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,15 +43,34 @@ module.exports = {
       },
     },
     {
-     resolve: `gatsby-plugin-google-analytics`,
-       options: {
+    resolve: `gatsby-plugin-google-gtag`,
+      options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
-         trackingId: "G-H728MH6PX9",
-         // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 325,
-        respectDNT: true,
-       },
+        trackingIds: [
+          "G-H728MH6PX9", // Google Analytics / GA
+        //"AW-CONVERSION_ID", // Google Ads / Adwords / AW
+        //"DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+       // gtagConfig: {
+        //  optimize_id: "OPT_CONTAINER_ID",
+        //  anonymize_ip: true,
+        //  cookie_expires: 0,
+        //},
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/data/**", "/components/**"],
+          // Defaults to https://www.googletagmanager.com
+          origin: "https://www.mramitpatel.com",
+        },
       },
+    },
     //{
       //resolve: `gatsby-plugin-manifest`,
       //options: {
