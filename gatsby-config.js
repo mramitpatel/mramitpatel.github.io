98e76d652f7be37b6bdd50338a16a8ea9dcdd4c5
Amit Dishoom <amit@mramitpatel.com>
2022-05-14 20:10:26 -0500

    gtag cookies

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,11 +53,11 @@ module.exports = {
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
-       // gtagConfig: {
+       gtagConfig: {
         //  optimize_id: "OPT_CONTAINER_ID",
         //  anonymize_ip: true,
-        //  cookie_expires: 0,
-        //},
+          cookie_expires: 0,
+        },
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
