c574ca1c521451b866fe7517b027d6b3d29ccaad
Amit Dishoom <amit@mramitpatel.com>
2022-05-19 16:26:29 -0500

    gtag again

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,7 +71,7 @@ module.exports = {
         //  anonymize_ip: true,
           cookie_expires: 7200,
           cookie_domain: "https://www.mramitpatel.com/",
-          cookie_flags: "SameSite=None;Secure",
+        //  cookie_flags: "SameSite=None;Secure",
         },
         // This object is used for configuration specific to this plugin
         pluginConfig: {
