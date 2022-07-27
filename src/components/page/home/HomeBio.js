78dafd0c74f5a5d0599004edc82c2be908074880
Amit Dishoom <amit@mramitpatel.com>
2022-07-26 22:36:25 -0500

    removed debug

--- src/components/page/home/HomeBio.js
+++ src/components/page/home/HomeBio.js
@@ -13,12 +13,12 @@ export default function Hero() {
 		<div className={`home-bio ${heroClass} col-lg-12`}>
 			<div className="container">
 				<div className="row">
-				<div className="content push-1 col-5 col-lg-5 col-md-12 col-sm-12 col-xl-5 ">
+				<div className="content col-6 col-lg-5 col-md-12 col-sm-12 col-xl-5 ">
 							<h3>For over a decade, I've applied human-centered design to help teams solve complex challenges.</h3>
 							<Link to="/info" className="button button--invert">More about me</Link>
 					</div>
 
-					<img className="content col-6 col-sm-12 col-md-12 col-lg-6 col-xl-6" src={Hcd} alt="Border"/>
+					<img className="content col-5 col-sm-12 col-md-12 col-lg-6 col-xl-6" src={Hcd} alt="Border"/>
 
 
 				</div>
